e057a33e243e2f9e18fb329bd5815a2c744809f8
maRia-1506A <zannatulfarzana363@gmail.com>
2026-01-10 23:45:52 +0600

    half done question

--- CASE_BASED_QUESTION/solve.py
+++ CASE_BASED_QUESTION/solve.py
@@ -1,1 +1,17 @@
 import pandas as pd
+
+# Customer Profile DataFrame
+customer_profile = pd.DataFrame(
+    {
+        'CustomerID': [101, 102, 103],
+        'Name': ['Maria', 'Afridi', 'Efath'],
+        'City': ['Jashore', 'Mymensingh', 'Bhola']
+    }
+)
+
+# Transaction History DataFrame
+transaction_history= pd.DataFrame(
+    {
+        
+    }
+)

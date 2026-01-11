6c59d33cb121eba5f732b09fdb7a59690b156775
maRia-1506A <zannatulfarzana363@gmail.com>
2026-01-12 00:06:24 +0600

    ques solved

--- CASE_BASED_QUESTION/solve.py
+++ CASE_BASED_QUESTION/solve.py
@@ -10,8 +10,32 @@ customer_profile = pd.DataFrame(
 )
 
 # Transaction History DataFrame
-transaction_history= pd.DataFrame(
+transaction_history = pd.DataFrame(
     {
-        
+        'CustomerID': [101, 102, 101, 103],
+        'TransactionID': [1, 2, 3, 4],
+        'Amount': [600, 500, 450, 700]
     }
 )
+
+# 1. Merge using the CustomerID column
+merge_df= pd.merge(customer_profile, transaction_history, how='inner', on='CustomerID')
+print('Merge using the CustomerID column')
+print(merge_df)
+
+# 2. Create a new customer record
+new_customer= pd.DataFrame({
+    'CustomerID': [104],
+    'Name': ['Fathum'],
+    'City': ['Kurigram'],
+    'TransactionID': [5],
+    'Amount': [1000]
+})
+
+#  3. Concatenate this new customer record with the merged DataFrame
+concat_df= pd.concat([merge_df, new_customer], ignore_index=True)
+print('\nConcatenate the new customer record with the merged DataFrame')
+print(concat_df)
+
+# 4. Save the final DataFrame to a CSV file.
+concat_df.to_csv('I:\CODE\Pandas\CASE_BASED_QUESTION\Final_customer_df.csv', index=False)
